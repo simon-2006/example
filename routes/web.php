912e67b9fed48b32ad1a1ceeed35f63af84caaf5
Simon <341037@student.mboutrecht.nl>
2025-09-17 10:06:23 +0200

    view

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,31 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/jobs', function () {
+        return view('jobs', [
+        'jobs' => [
+            [
+                'title' => 'Director ',
+                'salary' => '$50,000',
+            ],
+            [
+                'title' => 'programmer',
+                'salary' => '$20,000',
+        ],
+            [
+                'title' => 'Teacher',
+                'salary' => '$30,000',
+        ],
+
+        ]
+    ]);
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+
