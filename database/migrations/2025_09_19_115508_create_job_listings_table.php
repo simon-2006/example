e0a8a4794978b6b4078df970e088cc7a6352d88c
Simon <341037@student.mboutrecht.nl>
2025-09-24 10:06:44 +0200

    day 9

--- database/migrations/2025_09_19_115508_create_job_listings_table.php
+++ database/migrations/2025_09_19_115508_create_job_listings_table.php
@@ -11,8 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('jobs', function (Blueprint $table) {
+        Schema::create('job_listings', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('salary');
             $table->timestamps();
         });
     }
@@ -22,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('jobs');
+        Schema::dropIfExists('job_listings');
     }
 };
