99db06bf91567a476d6684e5ec4c6b1f73ed1240
Simon <341037@student.mboutrecht.nl>
2025-10-03 13:27:25 +0200

    dag 11

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -12,7 +12,10 @@ class Job extends Model {
 
     protected $fillable = ['title', 'salary'];
 
-    
+    public function employer() 
+    {
+        return $this->belongsTo(Employer::class);
+    }
 
-  
 }
+
