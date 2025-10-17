324bc03bf028c0cfd2d717997754debf5b1a60ac
Simon <341037@student.mboutrecht.nl>
2025-10-17 13:44:32 +0200

    day 13

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -2,14 +2,16 @@
     <x-slot:heading>
       jobs listings
     </x-slot:heading>
-
-    <ul>
+    <div class="space-y-4">
         @foreach ( $jobs as $job )
-          <li>
-              <a href="/jobs/ {{ $job['id'] }}"class= "text-blue-500 hover:underline">
+              <a href="/jobs/ {{ $job['id'] }}"class="hover:underline block px-4 py-6 border border-gray-200 rounded-lg">
+                <div class="font-bold text-blue-500 text-sm"> 
+                  {{ $job->employer->name }}
+                </div>
+                <div>
                <strong>{{ $job['title'] }}:</strong> pays {{ $job['salary']}} per year.
+                </div>
                </a>
-          </li>
         @endforeach
-    </ul>
+    </div>
 </x-layout>
